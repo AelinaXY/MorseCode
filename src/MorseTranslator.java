48538de6befd9c12047d634d2b88fc41dd1f8ba4
Stormstar109-new <Lauren@2infinity.net>
2023-03-21 15:33:19 +0000

    Add Initial Morse Translation

---
+++ src/MorseTranslator.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MorseTranslator {
+
+    private final Map<String,String> morseMap;
+    
+    public MorseTranslator()
+    {
+        morseMap = new HashMap<>();
+
+        morseMap.put(".-", "a");
+        morseMap.put("-...", "b");
+        morseMap.put("-.-.", "c");
+        morseMap.put("-..", "d");
+        morseMap.put(".", "e");
+
+        morseMap.put("..-.", "f");
+        morseMap.put("--.", "g");
+        morseMap.put("....", "h");
+        morseMap.put("..", "i");
+        morseMap.put(".---", "j");
+
+        morseMap.put("-.-", "k");
+        morseMap.put(".-..", "l");
+        morseMap.put("--", "m");
+        morseMap.put("-.", "n");
+        morseMap.put("---", "o");
+
+        morseMap.put(".--.", "p");
+        morseMap.put("--.-", "q");
+        morseMap.put(".-.", "r");
+        morseMap.put("...", "s");
+        morseMap.put("-", "t");
+
+        morseMap.put("..-", "u");
+        morseMap.put("...-", "v");
+        morseMap.put(".--", "w");
+        morseMap.put("-..-", "x");
+        morseMap.put("-.--", "y");
+
+        morseMap.put("--..", "z");
+        morseMap.put("/", " ");
+
+
+        morseMap.put(".----", "1");
+        morseMap.put("..---", "2");
+        morseMap.put("...--", "3");
+        morseMap.put("....-", "4");
+        morseMap.put(".....", "5");
+        morseMap.put("-....", "6");
+        morseMap.put("--...", "7");
+        morseMap.put("---..", "8");
+        morseMap.put("----.", "9");
+        morseMap.put("-----", "0");
+
+
+
+
+
+
+    }
+
+
+    public char[] translate(String string) {
+        return null;
+    }
+
+}
